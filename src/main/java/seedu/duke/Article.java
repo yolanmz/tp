4a9cfe4923c31344ee23224f5e1ad4cf6c888755
Yolanda <yolanda993322@gmail.com>
2020-03-09 07:39:09 -0400

    add features

---
+++ src/main/java/seedu/duke/Article.java
@@ -0,0 +1,18 @@
+package seedu.duke;
+
+public class Article {
+
+    public static  String name;
+    public static String url;
+
+
+    public Article(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+
+    public String toString(){
+        return (name + ": " + url);
+    }
+}
