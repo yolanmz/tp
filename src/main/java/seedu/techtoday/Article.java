148f64b54370bd2b9137e3574c9c6303065a6d36
katelo731 <xe870731@gmail.com>
2020-03-10 22:34:27 +0800

    fix javadoc bug.

--- src/main/java/seedu/techtoday/Article.java
+++ src/main/java/seedu/techtoday/Article.java
@@ -9,8 +9,13 @@ public class Article {
     public static String name;
     public static String url;
     public static String category;
-
-
+    
+    /**
+     * Constructor for article class.
+     * @param name - The article title given by the user.
+     * @param url - The url given by the user.
+     * @param category - The category set as default initially.
+     */
     public Article(String name, String url, String category) {
         this.name = name;
         this.url = url;
