4366cb96e43f9bd4ed39063c10e8a59cb6df1997
Alaukik <alaukik.pant@u.yale-nus.edu.sg>
2020-03-25 12:52:08 +0800

    changes to lines

--- src/main/java/seedu/techtoday/common/Messages.java
+++ src/main/java/seedu/techtoday/common/Messages.java
@@ -6,10 +6,8 @@ public class Messages {
     public static String SPLIT_LINE = "______________________________________________________________";
 
 
-    public static String LINE = "     ______________________________________________" +
-            "____________________________________";
 
-      /** Let the statement be printed in center. **/
+    /** Let the statement be printed in center. **/
     public static void printInCenter(@NotNull String str) {
         int left = (83 - str.length()) / 2;
         int right = 83 - left - str.length();
@@ -24,7 +22,7 @@ public class Messages {
 
     /** Show a ASCII code message to user. **/
     public static void greet() {
-        System.out.println(LINE);
+        printInCenter(SPLIT_LINE);
         printInCenter("");
         printInCenter("Hello! Here's TechToday.");
         printInCenter("Let me show you some technology news to refresh your mind!");
@@ -38,7 +36,7 @@ public class Messages {
         printInCenter("6. create [article / job / note]");
         printInCenter("7. exit");
         printInCenter("");
-        System.out.println(LINE);
+        printInCenter(SPLIT_LINE);
         System.out.println("     What can I do for you?");
         System.out.println();
     }
