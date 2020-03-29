1d4c46859344a6a452b9843378d30ef7f9faef25
Alaukik <alaukik.pant@u.yale-nus.edu.sg>
2020-03-29 17:59:51 +0800

    Deals with Empty Article Lists.

--- src/main/java/seedu/techtoday/joblist/JobListPrinter.java
+++ src/main/java/seedu/techtoday/joblist/JobListPrinter.java
@@ -1,5 +1,6 @@
 package seedu.techtoday.joblist;
 
+import seedu.techtoday.common.Messages;
 import seedu.techtoday.objects.Job;
 import java.util.ArrayList;
 
@@ -9,6 +10,9 @@ public class JobListPrinter {
      * @param jobList List that stores the jobs mentioned until now.
      */
     public static void execute(ArrayList<Job> jobList) {
+        if (jobList.size() == 0) {
+            Messages.printInCenter("There is nothing in the list currently.");
+        }
         int taskCounter = 1;
         for (Job job : jobList) {
             JobPrinter.execute(taskCounter, job);
