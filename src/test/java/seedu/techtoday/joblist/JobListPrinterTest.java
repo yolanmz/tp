ae633c308cf443a6f6e759cfc2493f9bd03ace30
melylopez99 <mlopez99@bu.edu>
2020-03-28 20:29:03 -0700

    gradle changes

--- src/test/java/seedu/techtoday/joblist/JobListPrinterTest.java
+++ src/test/java/seedu/techtoday/joblist/JobListPrinterTest.java
@@ -8,7 +8,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JobListPrinterTest {
 
